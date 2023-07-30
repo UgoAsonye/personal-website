0cd294100ec86f009e817b22dc67d5d8d4c41013
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-07-30 15:28:44 -0500

    Starting Code

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,14 @@
+class HomepageController < ApplicationController
+  # def index
+  #   @homepages = Homepage.all
+  #   render :index
+  # end
+
+  # def show
+  #   @homepage = Homepage.find_by(id: params[:id])
+  #   render :show
+  # end
+
+  def home
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # get "homepages" => "homepages#index"
+  # get "/homepage/:id" => "homepage#show"
+  get "/homepage/home"
+end
